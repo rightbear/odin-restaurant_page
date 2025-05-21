74c464bd7489388db5c03b0a714b19cdf244168e
rightbear <jjj17041113@gmail.com>
2025-05-22 00:00:39 +0800

    refactor: add scripts in package.json, and split out webpack.config.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -20,10 +15,6 @@ module.exports = {
   ],
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
       {
         test: /\.html$/i,
         loader: "html-loader",
